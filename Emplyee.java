d856e8792d1bd11802ee06e3433686e54fe189e9
donaldfletcher11 <donaldfletcher11@outlook.com>
2024-02-04 06:55:47 -0800

    build start of employee class

---
+++ Emplyee.java
@@ -0,0 +1,15 @@
+public class Emplyee {
+    int patientHealth;
+
+    public Emplyee() {
+        this.patientHealth = 10;
+    }
+
+    public int getPatientHealth() {
+        return this.patientHealth;
+    }
+
+    public void setPatientHealth(int PatientHealth) {
+        this.patientHealth = patientHealth;
+    }
+}
